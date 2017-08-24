7b74ef3d7cf1058f30923c2b148d2381d0fecf63
Keval <kevalpatel2106@gmail.com>
2017-08-24 17:53:37 +0530

    Fixing run errors for java module emoji parser.

--- emoji-parser/src/main/java/com/kevalpatel2106/emojiparser/Utils.java
+++ emoji-parser/src/main/java/com/kevalpatel2106/emojiparser/Utils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Keval Patel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.kevalpatel2106.emojiparser;
 
 import java.io.BufferedWriter;
@@ -10,7 +26,7 @@ import java.util.ArrayList;
  * Created by Keval on 24-Aug-17.
  */
 class Utils {
-    private static final String CURRENT_DIR_PATH = System.getProperty("user.dir");
+    private static final String CURRENT_DIR_PATH = System.getProperty("user.dir") + "/emoji-parser/out";
 
     static String removeFirstEmojiFromText(String textWithEmojiFirst) {
         return textWithEmojiFirst.substring(textWithEmojiFirst.indexOf(" ") + 1);
@@ -20,8 +36,13 @@ class Utils {
         return textWithCodePoint.substring(textWithCodePoint.indexOf("+") + 1);
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     static File getImageDir(String vendor) {
-        File dir = new File(CURRENT_DIR_PATH + "/" + vendor);
+
+        File dir = new File(CURRENT_DIR_PATH);
+        if (!dir.exists()) dir.mkdir();
+
+        dir = new File(CURRENT_DIR_PATH + "/" + vendor);
         if (!dir.exists()) dir.mkdir();
         return dir;
     }
